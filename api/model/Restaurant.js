c3c03fe521a5ac2a201d56d3a25cdbf1483cf02d
vivian <vivian.v18723@gmail.com>
2022-08-08 23:17:14 +0100

    create static methods for get & post requests

--- api/model/Restaurant.js
+++ api/model/Restaurant.js
@@ -7,9 +7,48 @@ class Restaurant {
     this.price = data.price;
   }
 
-  static displayAllFood() {
-    const allFood = db.query("SELECT * FROM restaurant");
-    return allFood;
+  static async displayFullMenu() {
+    try {
+      const fullMenu = await db.query("SELECT * FROM restaurant;");
+      const restaurants = fullMenu.rows.map(
+        (singleRow) => new Restaurant(singleRow)
+      );
+      return restaurants;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  /* 
+    - SAME AS ABOVE BUT USES 'new Promise()':
+    static displayFullMenu() {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const fullMenu = await db.query("SELECT * FROM restaurant;");
+          const restaurants = fullMenu.rows.map(
+            (singleRow) => new Restaurant(singleRow)
+          );
+          resolve(restaurants);
+        } catch (err) {
+          console.log(err);
+          reject(err);
+        }
+      });
+    }
+*/
+
+  static async addMenuSlot(food, price) {
+    try {
+      const newMenuSlot = await db.query(
+        `INSERT INTO restaurant (food, price) VALUES($1, $2) RETURNING *;`,
+        [food, price]
+      );
+      const menu = new Restaurant(newMenuSlot.rows[0]);
+      return menu;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
   }
 }
 
