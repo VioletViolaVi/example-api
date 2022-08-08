08399729df53ba837e6fd58e7d2e2b5edab80231
vivian <vivian.v18723@gmail.com>
2022-08-08 22:36:24 +0100

    add restaurant table

--- db/migrations/1_setup.sql
+++ db/migrations/1_setup.sql
@@ -6,4 +6,14 @@ CREATE TABLE
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     capacity INT NOT NULL
-  )
+  );
+
+DROP TABLE
+  IF EXISTS restaurant;
+
+CREATE TABLE
+  restaurant (
+    id SERIAL PRIMARY KEY,
+    food VARCHAR(50) NOT NULL,
+    price INT NOT NULL
+  );
