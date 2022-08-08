708ca60f2e6d65cd82f894937816b9387cf99e5e
vivian <vivian.v18723@gmail.com>
2022-08-08 22:36:37 +0100

    add restaurant table data

--- db/migrations/2_seed.sql
+++ db/migrations/2_seed.sql
@@ -6,3 +6,12 @@ VALUES
   ('Norman', 26),
   ('Bertha', 42),
   ('Gordon', 12);
+
+INSERT INTO
+  restaurant (food, price)
+VALUES
+  ('Chips', 65),
+  ('Fish', 70),
+  ('Chicken', 55),
+  ('Rice', 10),
+  ('Pizza', 35);
